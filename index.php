860c72103d115076948c33bade23bcbc75f614d2
YuriiF228 <9026220@student.zadkine.nl>
2025-02-19 11:56:54 +0100

    Home Pagina
    Ik heb Bootstrap met fonts toegevoegd.
    LET OP. BOOTSTRAP IS TOEGEVOEGD

---
+++ index.php
@@ -0,0 +1,81 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700;800;900&family=Monomakh&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./fonts/kanit.scss">
+
+    <!-- Links -->
+     <link rel="stylesheet" href="./style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="kanit-regular">
+            <nav class="w-75 navbar navbar-expand-lg bg-transparent m-auto">
+                <div class="container-fluid">
+                    <a class="navbar-brand fs-5" href="#">Wild West</a>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fs-5" aria-current="page" href="#">Room 1</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fs-5" href="#">Room 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link fs-5" href="#">Room 3</a>
+                            </li>
+                        </ul>
+                        <span class="navbar-text">
+                            Made by Raf Danila Yurii.
+                        </span>
+                    </div>
+                </div>
+            </nav>
+            <div class="container mt-5 position-relative">
+                <div class="w-100 time-bg position-absolute"></div>
+
+                <div class="content">
+                    <div class="menubar d-flex justify-content-center align-items-center pt-5">
+                        <h2 class="m-5">About Game</h2>
+                        <h2 class="m-5">Rules</h2>
+                        <h2 class="m-5">Creators</h2>
+                    </div>
+
+                    <div class="aboutGame-Block">
+
+                    </div>
+
+                    <div class="rules-Block">
+
+                    </div>
+                    <div class="w-75 m-auto creators-Block d-flex justify-content-evenly">
+                        <ul>
+                            <li>Raf</li>
+                            <li>Github</li>
+                            <li>+31 6 12 34 56</li>
+                        </ul>
+                        <ul>
+                            <li>Danila</li>
+                            <li>Github</li>
+                            <li>+31 6 12 34 56</li>
+                        </ul>
+                        <ul>
+                            <li>Yurii</li>
+                            <li>Github</li>
+                            <li>Linkedin</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
